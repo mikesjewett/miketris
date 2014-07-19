dfc9e18d43145cae247fbd6338c95327f197ee1e
Mike Jewett <mikesjewett@gmail.com>
2014-07-19 10:21:09 -0400

    Added blocks

--- Miketris/GameScene.swift
+++ Miketris/GameScene.swift
@@ -8,11 +8,13 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
     
     init(size: CGSize) {
         super.init(size: size)
@@ -23,5 +25,25 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+
+        if lastTick == nil {
+            return
+        }
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate.date()
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate.date()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
